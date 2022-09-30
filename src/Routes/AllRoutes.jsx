6291497eab63a9a2cf179da1744376c0da8c5b12
amandk5 <adkofficial01@gmail.com>
2022-09-30 23:44:23 +0530

    day-4

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HelpAndSupportPage from "./HelpAndSupportPage";
 import HomePage from "./HomePage";
+import ResultsPage from "./ResultsPage";
 
 export default function AllRoutes(){
     return (
         <Routes>
             <Route path="/" element={<HomePage/>}/>
             <Route path="/faq" element={<HelpAndSupportPage/>}/>
+            <Route path="/search" element={<ResultsPage/>}/>
         </Routes>
     )
 }
--- src/Routes/HomePage.jsx
+++ src/Routes/HomePage.jsx
@@ -1,11 +1,9 @@
 import Box1 from "../Components/Box1";
-import Navbar from "../Components/Navbar";
 
 export default function HomePage(){
 
     return (
         <div>
-            <Navbar/>
             <Box1/>
         </div>
     )
